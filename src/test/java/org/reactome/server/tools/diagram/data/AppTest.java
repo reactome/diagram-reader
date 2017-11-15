e33cbbc3248671b0ae856bd90a38c80279dba9f4
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2017-11-15 16:48:25 +0000

    Tests updated

--- src/test/java/org/reactome/server/tools/diagram/data/AppTest.java
+++ src/test/java/org/reactome/server/tools/diagram/data/AppTest.java
@@ -13,11 +13,15 @@ import org.reactome.server.tools.diagram.data.profile.analysis.AnalysisProfile;
 import org.reactome.server.tools.diagram.data.profile.interactors.InteractorProfile;
 import sun.misc.IOUtils;
 
+import javax.net.ssl.*;
 import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 
 /**
  * Unit testing.
@@ -25,7 +29,7 @@ import java.nio.file.Paths;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AppTest extends TestCase {
 
-    private static String BASE_URL = "http://www.reactome.org/download/current/diagram/";
+    private static String BASE_URL = "https://reactome.org/download/current/diagram/";
     private static String DIAGRAM_ID = "R-HSA-186712"; //Stable Id or DBId
     private static String SUFFIX = ".json";
     private static String MODERN_PROFILE = "Modern";
@@ -38,6 +42,7 @@ public class AppTest extends TestCase {
     private static String INTERACTORS_PROFILE_1 = "interactor_profile_01";
     private static String INTERACTORS_PROFILE_2 = "interactor_profile_02";
 
+    static TrustManager[] trustAllCerts;
     /**
      * Create the test case
      *
@@ -56,70 +61,77 @@ public class AppTest extends TestCase {
         return new TestSuite( AppTest.class );
     }
 
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        overrideCertificateCheck();
+    }
+
     /**
      * Diagram Test :-)
      */
-    public void testDiagram()
-    {
+    public void testDiagram() {
+        System.out.print("Testing diagram[" + DIAGRAM_ID + "]...");
         String url = BASE_URL + DIAGRAM_ID + SUFFIX;
         try {
             String json = readFromURL(url);
             Diagram diagram = DiagramFactory.getDiagram(json);
             assertNotNull(diagram);
             assertEquals(DIAGRAM_ID, diagram.getStableId());
-            System.out.println("Testing diagram[" + DIAGRAM_ID + "] ... OK");
+            System.out.print("OK\n");
         } catch (IOException e) {
             fail("Could not retrieve diagram from: " + url);
-        } catch (DeserializationException e) {
             e.printStackTrace();
+        } catch (DeserializationException e) {
             fail();
+            e.printStackTrace();
         }
     }
 
     /**
      * Graph Test :-)
      */
-    public void testGraph()
-    {
+    public void testGraph() {
+        System.out.print("Testing graph[" + DIAGRAM_ID + "] ...");
         String url = BASE_URL + DIAGRAM_ID + ".graph" + SUFFIX;
         try {
             String json = readFromURL(url);
             Graph graph = DiagramFactory.getGraph(json);
             assertNotNull(graph);
-            System.out.println("Testing graph[" + DIAGRAM_ID + "] ... OK");
+            System.out.print("OK\n");
         } catch (IOException e) {
             fail("Could not retrieve graph from: " + url);
-        } catch (DeserializationException e) {
             e.printStackTrace();
+        } catch (DeserializationException e) {
             fail();
+            e.printStackTrace();
         }
     }
 
     /**
-     * Profile Test :-)
+     * Profile Tests :-)
      */
-    public void testDiagramProfile()
-    {
+    public void testDiagramProfile() {
         String profileName = MODERN_PROFILE + ".json";
+        System.out.println("Testing diagram colour profile[" + profileName + "] ...");
         try {
             String json = readFile(profileName);
             DiagramProfile profile = DiagramFactory.getProfile(json);
             assertNotNull(profile);
             assertEquals(MODERN_PROFILE, profile.getName());
-            System.out.println("Testing diagram colour profile[" + profileName + "] ... OK");
+            System.out.print("OK");
         } catch (IOException e) {
             fail("Could not retrieve profile: " + profileName);
         } catch (DeserializationException e) {
-            e.printStackTrace();
             fail();
+            e.printStackTrace();
         } catch (URISyntaxException e) {
-            e.printStackTrace();
             fail();
+            e.printStackTrace();
         }
     }
 
-    public void testAnalysisProfile()
-    {
+    public void testAnalysisProfile() {
         String profileName = ANALYSIS_PROFILE_1 + ".json";
         try {
             String json = readFile(profileName);
@@ -129,16 +141,15 @@ public class AppTest extends TestCase {
         } catch (IOException e) {
             fail("Could not retrieve profile: " + profileName);
         } catch (DeserializationException e) {
-            e.printStackTrace();
             fail();
+            e.printStackTrace();
         } catch (URISyntaxException e) {
-            e.printStackTrace();
             fail();
+            e.printStackTrace();
         }
     }
 
-    public void testInteractorsProfile()
-    {
+    public void testInteractorsProfile() {
         String profileName = INTERACTORS_PROFILE_1 + ".json";
         try {
             String json = readFile(profileName);
@@ -156,12 +167,59 @@ public class AppTest extends TestCase {
         }
     }
 
-    private static String readFromURL(String url) throws IOException {
-        InputStream in = new URL(url).openStream();
+    private static String readFromURL(String endpoint) throws IOException {
+        URL url = new URL(endpoint);
+        HttpsURLConnection httpURLConnection  = (HttpsURLConnection) url.openConnection();
+        httpURLConnection.connect();
+        InputStream in = httpURLConnection.getInputStream();
         return new String(IOUtils.readFully(in, -1, true));
     }
 
     private static String readFile(String filename) throws IOException, URISyntaxException {
         return new String(Files.readAllBytes(Paths.get(AppTest.class.getResource(filename).toURI())));
+    }
+
+    /**
+     *  Overrides the check and accept an untrusted certificate
+     *
+     *  Fix for
+     *  Exception in thread "main" javax.net.ssl.SSLHandshakeException:
+     *  sun.security.validator.ValidatorException:
+     *           PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException:
+     *               unable to find valid certification path to requested target
+     */
+    private static void overrideCertificateCheck(){
+        try {
+            trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return null;
+                        }
+
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {  }
+
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {  }
+
+                    }
+            };
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Create all-trusting host name verifier
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            };
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
